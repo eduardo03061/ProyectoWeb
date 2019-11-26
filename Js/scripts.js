28834b414d6e4a1fc91950d98ea24a4329aa176b
Eduardo Hernandez <eduardoalejandrohg@gmail.com>
2019-11-25 18:26:43 -0600

    Nueva version

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,4 +1,6 @@
 jQuery('document').ready(function($){
+    $("#seccion1").css({"height":$(window).height()+"px"});
+
     var MenuBtn = $('.menu-icon'),
     menu=$('.navigation ul');
 
@@ -10,6 +12,19 @@ jQuery('document').ready(function($){
             menu.addClass('show');
         }
     });
+    
+    var Opc = $('.opc'),
+    opcs=$('.navigation ul li ul li a');
+
+    Opc.click(function(){
+        if(opcs.hasClass('show')){
+            opcs.removeClass('show');
+        }else{
+            opcs.addClass('show');
+        }
+    });
+
+
     
     
 });
